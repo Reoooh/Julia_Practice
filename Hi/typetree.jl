d48e895b301a8284ac7284ff8269cba1b16457f7
reoh <1750913582@qq.com>
2020-10-02 01:14:32 +0800

    10-01-20

---
+++ Hi/typetree.jl
@@ -0,0 +1,10 @@
+function showTypeTree(T, level = 0)
+    println("\t" ^ level, T)
+    for t in subtypes(T)
+        if t != Any
+            showTypeTree(t, level + 1)
+        end
+    end
+end
+
+showTypeTree(Real)
